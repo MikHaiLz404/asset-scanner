c8a2375626021e98e063d7f5297c32636729cb94
Jojo - Ekaphop <ekaphop_won@truegamestudio.com>
2025-12-02 22:47:16 +0700

    feat: Add AVI and MOV to supported video asset extensions.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,13 @@ export const ALLOWED_EXTENSIONS = [
     '.png', '.jpg', '.jpeg', '.tga', '.tif', '.tiff', '.bmp', '.psd',
     '.fbx', '.obj', '.gltf', '.glb', '.ma', '.mb', '.blend',
     '.mp3', '.wav', '.ogg',
-    '.mp4', '.webm'
+    '.mp4', '.webm', '.avi', '.mov'
 ]
 
 export const MODEL_EXTENSIONS = ['.fbx', '.obj', '.gltf', '.glb', '.ma', '.mb', '.blend']
 export const IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.tga', '.tif', '.tiff', '.bmp', '.psd']
 export const AUDIO_EXTENSIONS = ['.mp3', '.wav', '.ogg']
-export const VIDEO_EXTENSIONS = ['.mp4', '.webm']
+export const VIDEO_EXTENSIONS = ['.mp4', '.webm', '.avi', '.mov']
 
 export const IGNORED_FOLDERS = [
     'node_modules', '.git', '.vscode', '.idea', '.vs', // IDEs
